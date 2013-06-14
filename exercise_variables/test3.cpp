d7a4b03e2f6266dc375a0778309d984e20147a33
Philipp Neugebauer <flippus@nexgo.de>
2013-06-15 00:57:11 +0200

    computations added

--- exercise_variables/test3.cpp
+++ exercise_variables/test3.cpp
@@ -3,21 +3,38 @@
 using namespace std;
 
 int main(){
-    int a, *b, c=&a, *d, f, g=&f;
+    // can't work, becuase  c and g aren't pointers
+    //int a, *b, c=&a, *d, f, g=&f;
+    int a, *b, c, *d, f, g;
     c = 10;
-    b = 4;
+    // can't work, b is a pointer
+    //b = 4;
+    // a = random number?!
     a = a + 1;
-    f = a + b;
+    //impossible, b is a pointer
+    //f = a + b;
+    // d = 0
     d = new int;
+    // d = 20
     *d = 20;
+    // g = 0 + a
     g = g + a;
+    // b points now to the memory position of a; b = a
     b = &a;
-    *b = d* + g;
+    // b = 20 + a
+    *b = *d + g;
+    // f = random number
     f++;
+    // d points now to the memory position of f; d = f
     d = &f;
-    b = d;
+    // impossible, d is a pointer
+    //b = d;
+    // value of b is now random
     *b = a + f;
+    // d = random
     *d = *d + a;
+    // f = random
     f = a + *b + *d +c;
+    // c = 11
     c++;
 }
